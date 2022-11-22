65d6bf81e50307897aed7e8c36c4a4c939883642
paul moore <in2scripts@yahoo.com>
2022-11-22 16:22:20 +0000

    Initial Commit

---
+++ wkwebview/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  wkwebview
+//
+//  Created by Afrar Malakooth on 6/5/20.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
